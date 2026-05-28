a0f9318a4335ef078bcee93f0258e5a7c5be3e08
Olivera Leonardo <leoliveradev@gmail.com>
2026-05-28 16:22:44 -0300

    refactor(query): remove unused filter interfaces and related query function

--- src/helpers/query.ts
+++ src/helpers/query.ts
@@ -3,33 +3,6 @@ import type { Database } from '../types/database.js'
 
 type TableName = keyof Database['public']['Tables']
 
-// interface PeriodFilters {
-//   anio?: string
-//   trimestre?: string
-// }
-
-// interface ProvinciaFilters extends PeriodFilters {
-//   provincia?: string
-// }
-
-// export const queryWithProvincia = async (
-//   table: TableName,
-//   filters: ProvinciaFilters,
-//   orderBy: string[] = ['anio', 'trimestre']
-// ) => {
-//   let query = supabase.from(table).select('*') as any
-
-//   if (filters.anio) query = query.eq('anio', Number(filters.anio))
-//   if (filters.trimestre) query = query.eq('trimestre', Number(filters.trimestre))
-//   if (filters.provincia) query = query.ilike('provincia', `%${filters.provincia}%`)
-
-//   for (const col of orderBy) {
-//     query = query.order(col)
-//   }
-
-//   return query
-// }
-
 export const queryWithFilters = async (
   table: TableName,
   filters: Record<string, any>,
