1303534c8a5a7770ec9a4ba97ca13e19e86c9987
Olivera Leonardo <leoliveradev@gmail.com>
2026-09-01 13:01:50 -0300

    feat(query): add new filters for localidad and tecnologia in query adapters

--- src/controllers/adapters/query.adapters.ts
+++ src/controllers/adapters/query.adapters.ts
@@ -1,11 +1,11 @@
 import { queryWithFilters } from '../../helpers/query.js'
-import type { PeriodMonthsQueryParams, PeriodQueryParams } from '../../types/index.js'
+import type { LocalidadTecnologiaQueryParams, PeriodMonthsQueryParams, PeriodQueryParams } from '../../types/index.js'
 import type { Database } from '../../types/database.js'
 
 type TableName = keyof Database['public']['Tables']
 
 export const withMonth = (
-  table: TableName, 
+  table: TableName,
   filters: PeriodMonthsQueryParams
 ) =>
   queryWithFilters(table, filters, ['anio', 'mes'])
@@ -22,3 +22,38 @@ export const withProvince = (
   options?: { count?: boolean }
 ) =>
   queryWithFilters(table, filters, ['anio', 'trimestre'], options)
+
+export interface LocalidadQueryParams {
+  provincia?: string
+  localidad?: string
+}
+
+export const withVelocidadLocalidad = (
+  table: TableName,
+  filters: LocalidadQueryParams & {
+    from?: number
+    to?: number
+  },
+  options?: { count?: boolean }
+) =>
+  queryWithFilters(
+    table,
+    filters,
+    ['provincia', 'localidad'],
+    options
+  )
+
+export const withTecnologiaLocalidad = (
+  table: TableName,
+  filters: LocalidadTecnologiaQueryParams & {
+    from?: number
+    to?: number
+  },
+  options?: { count?: boolean }
+) =>
+  queryWithFilters(
+    table,
+    filters,
+    ['provincia', 'localidad', 'tecnologia'],
+    options
+  )
